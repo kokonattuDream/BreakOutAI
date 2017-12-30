1e6dcd3236242081a56ac5a8a760a6713972895c
kokonattuDream <32606269+kokonattuDream@users.noreply.github.com>
2017-12-30 10:18:20 -0500

    Parameters needed for exploring

---
+++ main.py
@@ -0,0 +1,13 @@
+class Params():
+    """
+    Gathering all the parameters that are required to explore
+    """
+    def __init__(self):
+        self.lr = 0.0001
+        self.gamma = 0.99
+        self.tau = 1.
+        self.seed = 1
+        self.num_processes = 16
+        self.num_steps = 20
+        self.max_episode_length = 10000
+        self.env_name = 'Breakout-v0'
