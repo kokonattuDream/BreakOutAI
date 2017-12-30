9ae04cd23b5dbf192cfa96749debb141def73a44
kokonattuDream <32606269+kokonattuDream@users.noreply.github.com>
2017-12-30 13:37:39 -0500

    Main part

--- main.py
+++ main.py
@@ -11,3 +11,36 @@ class Params():
         self.num_steps = 20
         self.max_episode_length = 10000
         self.env_name = 'Breakout-v0'
+        
+# Main run
+
+#1 thread per core
+os.environ['OMP_NUM_THREADS'] = '1'
+params = Params()
+torch.manual_seed(params.seed)
+env = create_atari_env(params.env_name)
+
+#shared_model is the model shared by the different agents (different threads in different cores)
+shared_model = ActorCritic(env.observation_space.shape[0], env.action_space) 
+shared_model.share_memory() 
+
+optimizer = my_optim.SharedAdam(shared_model.parameters(), lr=params.lr)
+#store in shared memory
+optimizer.share_memory() 
+
+#Processes
+processes = []
+p = mp.Process(target=test, args=(params.num_processes, params, shared_model))
+p.start()
+processes.append(p)
+
+#Run all the other processes that will be trained by updating the shared model
+for rank in range(0, params.num_processes):
+    p = mp.Process(target=train, args=(rank, params, shared_model, optimizer))
+    p.start()
+    processes.append(p)
+    
+#Creating a pointer that will allow to kill all the threads when at least one of the threads,
+#or main.py will be killed, allowing to stop the program safely
+for p in processes:
+    p.join()
